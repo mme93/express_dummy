bc84abfdbc37389e13cd54d40cb9dc4f58154ad3
magantus@googlemail.com <Extrem93!>
2024-07-13 21:24:24 +0200

    update

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'test.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
